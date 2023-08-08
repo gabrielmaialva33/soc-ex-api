91081e8eda1bedfdd91b64ad6b403581a55ad4e3
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-08-08 20:09:21 -0300

    chore: update libs

--- mix.exs
+++ mix.exs
@@ -35,15 +35,15 @@ defmodule SocExApi.MixProject do
       {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.17.2"},
       {:phoenix_live_dashboard, "~> 0.8.0"},
       {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"},
+      {:finch, "~> 0.16"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
+      {:gettext, "~> 0.23"},
+      {:jason, "~> 1.4"},
+      {:plug_cowboy, "~> 2.6"},
       {:argon2_elixir, "~> 3.1"}
     ]
   end
