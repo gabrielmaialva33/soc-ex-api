3a5ce743852cad245853dc3a7b8851f7c3306d21
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-08-08 01:29:34 -0300

    refactor: add global att in user schema

--- lib/soc_ex_api/accounts/user.ex
+++ lib/soc_ex_api/accounts/user.ex
@@ -2,13 +2,17 @@ defmodule SocExApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @required_fields ~w(username first_name last_name email password is_online is_deleted)a
+  @optional_fields ~w(is_online is_deleted)a
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "users" do
-    field :username, :string
     field :first_name, :string
     field :last_name, :string
+    field :username, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :is_online, :boolean, default: false
     field :is_deleted, :boolean, default: false
@@ -19,23 +23,7 @@ defmodule SocExApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [
-      :first_name,
-      :last_name,
-      :email,
-      :username,
-      :password_hash,
-      :is_online,
-      :is_deleted
-    ])
-    |> validate_required([
-      :first_name,
-      :last_name,
-      :email,
-      :username,
-      :password_hash,
-      :is_online,
-      :is_deleted
-    ])
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
   end
 end
