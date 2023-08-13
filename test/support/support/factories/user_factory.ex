2078bb8c9118e75acb4f8f751ca2a39c30281de2
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-08-13 00:53:43 -0300

    refactor: user factory

--- test/support/support/factories/user_factory.ex
+++ test/support/support/factories/user_factory.ex
@@ -5,6 +5,7 @@ defmodule SocExApi.UserFactory do
   """
 
   alias SocExApi.Accounts.User
+  alias SocExApi.Repo
 
   defmacro __using__(_) do
     quote do
@@ -14,17 +15,32 @@ defmodule SocExApi.UserFactory do
           first_name: Faker.Person.first_name(),
           last_name: Faker.Person.last_name(),
           email: Faker.Internet.email(),
-          password_hash: Argon2.hash_pwd_salt(Faker.Internet.password()),
-          is_online: Faker.Boolean.boolean(),
-          is_deleted: Faker.Boolean.boolean()
+          password_hash: Argon2.hash_pwd_salt("Soc@551238"),
+          is_online: Faker.random_uniform() > Faker.random_uniform(),
+          is_deleted: Faker.random_uniform() > Faker.random_uniform()
         }
       end
     end
   end
 
+  def user_factory do
+    %User{
+      username: Faker.Internet.user_name(),
+      first_name: Faker.Person.first_name(),
+      last_name: Faker.Person.last_name(),
+      email: Faker.Internet.email(),
+      password_hash: Argon2.hash_pwd_salt("Soc@551238"),
+      is_online: Faker.random_uniform() > Faker.random_uniform(),
+      is_deleted: Faker.random_uniform() > Faker.random_uniform()
+    }
+  end
+
+  @spec create_user(any) :: any
   def create_user(attrs \\ %{}) do
-    %User{}
-    |> Ecto.build_assoc(:user, Map.merge(user_factory(), attrs))
+    user = user_factory()
+
+    user
+    |> User.changeset(attrs)
     |> Repo.insert!()
   end
 
